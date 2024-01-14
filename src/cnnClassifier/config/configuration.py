dd0208f7842c83e7e84f982a4cec398159ea4d4e
JanvitaReddy11 <tamu2023@tamu.edu>
2024-01-13 22:04:04 -0600

    Training added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,25 +1,43 @@
-from cnnClassifier.constants import CONFIG_FILE_PATH ,PARAMS_FILE_PATH
-from cnnClassifier.utils.common import *
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig)
+from cnnClassifier.constants import *
+import os
+from pathlib import Path
+from cnnClassifier.utils.common import read_yaml, create_directories
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig)
+
+
 
 class ConfigurationManager:
-    def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filename= PARAMS_FILE_PATH):
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+
         self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_filename)
+        self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
+
+    
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
+
         create_directories([config.root_dir])
+
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
             source_URL=config.source_URL,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir
+            unzip_dir=config.unzip_dir 
         )
 
-        return data_ingestion_config  
+        return data_ingestion_config
+    
+
+
     
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
@@ -41,7 +59,6 @@ class ConfigurationManager:
     
 
 
-    
     def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
         config = self.config.prepare_callbacks
         model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
@@ -57,4 +74,32 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
 
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.training_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
+    
+
+
+
+  
